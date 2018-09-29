36c30d98811dd1e4b68d84c67d46ccb5a1af6871
gyfastas <35861032+gyfastas@users.noreply.github.com>
2018-09-29 19:39:22 +0800

    reset

--- version2_3_4.py
+++ version2_3_4.py
@@ -1,8 +1,17 @@
 from __future__ import print_function
 import sys
 from PyQt5 import QtCore, QtGui,QtWidgets
+<<<<<<< HEAD
+<<<<<<< HEAD
+
 from PyQt5.QtCore import QPropertyAnimation, QEasingCurve, QRect
-from PyQt5.QtWidgets import QApplication, QLineEdit, QInputDialog, QGridLayout, QLabel, QPushButton, QFrame, QWidget,QMenu
+from PyQt5.QtWidgets import QApplication, QLineEdit, QInputDialog, QGridLayout, QLabel, QPushButton, QFrame, QWidget,QMenu, QMainWindow
+=======
+from PyQt5.QtWidgets import QApplication, QLineEdit, QInputDialog, QGridLayout, QLabel, QPushButton, QFrame
+>>>>>>> parent of d2af8f7... 把按钮改成了下拉菜单
+=======
+from PyQt5.QtWidgets import QApplication, QLineEdit, QInputDialog, QGridLayout, QLabel, QPushButton, QFrame
+>>>>>>> parent of d2af8f7... 把按钮改成了下拉菜单
 import os
 sys.setrecursionlimit(1000000)
 
@@ -124,10 +133,9 @@ def add_new_face(img, name):
     cv2.imwrite("./images/" + str(name) + '.jpg', img)
 
 
-class Ui_MainWindow(QtWidgets.QWidget):
-    def __init__(self, parent=None):
-        super(Ui_MainWindow, self).__init__(parent)
-
+class Ui_MainWindow(QMainWindow):
+    def __init__(self):
+        super(Ui_MainWindow, self).__init__()
         # self.face_recong = face.Recognition()
         self.timer_camera = QtCore.QTimer()
         self.cap = cv2.VideoCapture()
@@ -137,58 +145,109 @@ class Ui_MainWindow(QtWidgets.QWidget):
         self.__flag_work = 0
         self.x =0
         self.recognition_flag=False
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of a1b7a98... 换成了下拉菜单
         self.initMenu()
         self.initAnimation()
+
+=======
+>>>>>>> parent of d2af8f7... 把按钮改成了下拉菜单
+=======
+>>>>>>> parent of d2af8f7... 把按钮改成了下拉菜单
     def set_ui(self):
+        self.resize(1000,800)
+
+<<<<<<< HEAD
+        #加一个窗口
+        self.centralwidget = QtWidgets.QWidget()
+        self.centralwidget.resize(800,600)
         self.nameLable = QLabel(" ")
-        self.__layout_main = QtWidgets.QHBoxLayout()
-        self.__layout_fun_button = QtWidgets.QVBoxLayout()
-        self.__layout_data_show = QtWidgets.QVBoxLayout()
+        self.centralwidget.__layout_main = QtWidgets.QHBoxLayout()
+        self.centralwidget.__layout_fun_button = QtWidgets.QVBoxLayout()
+        self.centralwidget.__layout_data_show = QtWidgets.QVBoxLayout()
+        ###为显示图像位置添加Label
+=======
+
+
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> parent of d2af8f7... 把按钮改成了下拉菜单
+        self.button_open_camera = QtWidgets.QPushButton(u'Open camera')
+        self.button_close = QtWidgets.QPushButton(u'exit')
 
         
+        self.button_detect=QtWidgets.QPushButton(u'recognition')
+<<<<<<< HEAD
+=======
+        
+        self.button_record=QtWidgets.QPushButton(u'record ')
+
+      
+        self.button_writeface=QtWidgets.QPushButton(u'Add a new face')
+
+        self.button_open_camera.setMinimumHeight(50)
+        self.button_close.setMinimumHeight(50)
+
+        self.button_detect.setMinimumHeight(50)
+        self.button_close.move(10,100)
+
+>>>>>>> parent of d2af8f7... 把按钮改成了下拉菜单
+        
+        self.button_record=QtWidgets.QPushButton(u'record ')
+
+      
+        self.button_writeface=QtWidgets.QPushButton(u'Add a new face')
+
+        self.button_open_camera.setMinimumHeight(50)
+        self.button_close.setMinimumHeight(50)
+
+        self.button_detect.setMinimumHeight(50)
+        self.button_close.move(10,100)
+
+        
+>>>>>>> parent of d2af8f7... 把按钮改成了下拉菜单
         self.label_show_camera = QtWidgets.QLabel()
         self.label_move = QtWidgets.QLabel()
         self.label_move.setFixedSize(200, 200)
-
-        
         self.label_show_camera.setFixedSize(800, 600)
         self.label_show_camera.setAutoFillBackground(False)
 
+<<<<<<< HEAD
+<<<<<<< HEAD
 
+        self.centralwidget.__layout_main.addWidget(self.centralwidget.label_show_camera)
+=======
+=======
+>>>>>>> parent of d2af8f7... 把按钮改成了下拉菜单
+        self.__layout_fun_button.addWidget(self.button_open_camera)
+        self.__layout_fun_button.addWidget(self.button_close)
+        self.__layout_fun_button.addWidget(self.label_move)
+        self.__layout_fun_button.addWidget(self.button_detect)
+        self.__layout_fun_button.addWidget(self.button_record)
+        self.__layout_fun_button.addWidget(self.button_writeface)
+<<<<<<< HEAD
+>>>>>>> parent of d2af8f7... 把按钮改成了下拉菜单
+=======
+>>>>>>> parent of d2af8f7... 把按钮改成了下拉菜单
 
-        self.__layout_main.addLayout(self.__layout_fun_button)
-        self.__layout_main.addWidget(self.label_show_camera)
+        self.centralwidget.label_move.raise_()
 
-        self.setLayout(self.__layout_main)
-        self.label_move.raise_()
-        self.setWindowTitle(u'test')
-
-    def contextMenuEvent(self, event):
-        pos = event.globalPos()
-        size = self._contextMenu.sizeHint()
-        x, y, w, h = pos.x(), pos.y(), size.width(), size.height()
-        self._animation.stop()
-        self._animation.setStartValue(QRect(x, y, 0, 0))
-        self._animation.setEndValue(QRect(x, y, w, h))
-        self._animation.start()
-        self._contextMenu.exec_(event.globalPos())
-
-    def initMenu(self):
-        self._contextMenu = QMenu(self)
-        self._contextMenu.addAction('打开相机', self.button_open_camera_click)
-        self._contextMenu.addAction('识别', self.button_detection_click)
-        self._contextMenu.addAction('记录', self.button_record_click)
-    def initAnimation(self):
-        # 按钮动画
-        self._animation = QPropertyAnimation(
-            self._contextMenu, b'geometry', self,
-            easingCurve=QEasingCurve.Linear, duration=300)
-        # easingCurve 修改该变量可以实现不同的效果
 
 
     def slot_init(self):
-
+        self.button_open_camera.clicked.connect(self.button_open_camera_click)
         self.timer_camera.timeout.connect(self.show_camera)
+        self.button_close.clicked.connect(self.close)
+        #add detection mode
+        self.button_detect.clicked.connect(self.button_detection_click)
+        #record connect
+        self.button_record.clicked.connect(self.button_record_click)
+        self.button_writeface.clicked.connect(self.button_wrtieface_click)
 
     def button_open_camera_click(self):
         if self.timer_camera.isActive() == False:
@@ -199,6 +258,8 @@ class Ui_MainWindow(QtWidgets.QWidget):
         
             else:
                 self.timer_camera.start(50)
+
+                self.button_open_camera.setText(u'close the camera')
         else:
             self.timer_camera.stop()
             self.cap.release()
@@ -213,7 +274,8 @@ class Ui_MainWindow(QtWidgets.QWidget):
         show = cv2.resize(self.image, (800, 600))
         show = cv2.cvtColor(show, cv2.COLOR_BGR2RGB)
         showImage = QtGui.QImage(show.data, show.shape[1], show.shape[0], QtGui.QImage.Format_RGB888)
-        self.label_show_camera.setPixmap(QtGui.QPixmap.fromImage(showImage))
+        #在相机窗口上显示
+        self.centralwidget.label_show_camera.setPixmap(QtGui.QPixmap.fromImage(showImage))
         # self.x += 1
         # self.label_move.move(self.x,100)
 
@@ -342,7 +404,9 @@ class Ui_MainWindow(QtWidgets.QWidget):
             cv2.putText(self.image, str('time:') + str(tx), (result[i]['box'][0] + 10, result[i]['box'][1] + 10),
                         cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 255), 1)
 
+
 app = QtWidgets.QApplication(sys.argv)
 ui = Ui_MainWindow()
 ui.show()
+ui.centralwidget.show()
 sys.exit(app.exec_())
